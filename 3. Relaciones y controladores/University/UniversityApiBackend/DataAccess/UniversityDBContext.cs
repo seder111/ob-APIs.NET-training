6d7e75eee7c3d8be71614869a45d9ad5f1e35786
sergio Gonz <sergiogonzalezmerino@gmail.com>
2022-07-15 16:23:49 +0200

    Training 2 and 3 renames folders

---
+++ 3. Relaciones y controladores/University/UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,22 @@
+﻿using UniversityApiBackend.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options) { }
+
+        public DbSet<User>? Users { get; set; }
+
+        public DbSet<Course>? Courses { get; set; }
+
+        public DbSet<Chapter>? Chapters { get; set; }
+
+        public DbSet<Student>? Students { get; set; }
+
+        public DbSet<Category>? Categories { get; set; }
+
+    }
+}
